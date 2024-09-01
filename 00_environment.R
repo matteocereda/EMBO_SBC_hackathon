cbbc630ec0dcc17c08aee09abb18f5c98f38d258
Mariachiara Grieco <61466632+mariachiaragrieco@users.noreply.github.com>
2024-09-01 14:43:40 +0200

    update 00_environment.R

--- 00_environment.R
+++ 00_environment.R
@@ -14,42 +14,13 @@
 
 
 
-if( Sys.info()['nodename']=='psychobook-2.local'){
-  
-}else if( Sys.info()['nodename']=='lab07.local'){
-  DATA_DIR='/Volumes/mc/repository/EMBO_hackathon'
-  CGB_DIR='/Volumes/mc/'
-  GIT_LOCAL_DIR='/Volumes/mc/repository/'
+if( Sys.info()['user'] == 'mgrieco'){
+  DATA_DIR='/Users/mgrieco/repo/EMBO_hackathon'
+  CGB_DIR='/Users/mgrieco/'
+  GIT_LOCAL_DIR='/Users/mgrieco/repo/'
   CGB_SHARED='~/Dropbox (HuGeF)/'
-  BEDTOOLS='/Applications/miniconda3/bin/bedtools'
+  BEDTOOLS='/Users/mgrieco/miniconda/bin/bedtools'
 
-}else if( Sys.info()['user'] =='mariachiara.grieco' ){
-  DATA_DIR='/hpcnfs/data/cgb/EMBO_hackathon/'
-  CGB_DIR='/hpcnfs/data/cgb/'
-  GIT_LOCAL_DIR='/hpcnfs/home/mariachiara.grieco/'
-  
-}else if( Sys.info()['user'] == 'mgrieco'){
-  DATA_DIR='/Users/mgrieco/repo/EMBO_hackathon'
-  CGB_DIR='/Users/mgrieco/'
-  GIT_LOCAL_DIR='/Users/mgrieco/repo/'
-  CGB_SHARED='~/Dropbox (HuGeF)/'
-  BEDTOOLS='/Users/mgrieco/miniconda/bin/bedtools'
-  
-}else if( Sys.info()['user'] == 'mgrieco'){
-  DATA_DIR='/Users/mgrieco/repo/EMBO_hackathon'
-  CGB_DIR='/Users/mgrieco/'
-  GIT_LOCAL_DIR='/Users/mgrieco/repo/'
-  CGB_SHARED='~/Dropbox (HuGeF)/'
-  BEDTOOLS='/Users/mgrieco/miniconda/bin/bedtools'
-  
-}else if( Sys.info()['nodename'] == 'cgb-tower'){
-  DATA_DIR='/home/matteo/Lavoro/EMBO_hackathon'
-  CGB_DIR='/home/matteo/Lavoro/'
-  GIT_LOCAL_DIR='/home/matteo/Lavoro/'
-  CGB_SHARED='/home/matteo/Dropbox (HuGeF)/'
-  BEDTOOLS='/usr/bin/bedtools'
-  
-  
   
   
 }else{
