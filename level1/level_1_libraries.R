dc906d47a220fdbb5267636c462385060ba46f43
Mariachiara Grieco <61466632+mariachiaragrieco@users.noreply.github.com>
2024-09-01 15:10:15 +0200

    Add files via upload

---
+++ level1/level_1_libraries.R
@@ -0,0 +1,43 @@
+## ***************************
+##
+## Script name: 00_libraries.R 
+## Purpose of script: Libraries needed for level 1   
+## Author: mg  
+## Date Created: 2024-08-14
+##
+## ***************************
+##
+## Notes: These are the packages used in the execution of level 1. In 00_dependencies.R are specified other packages needed for their installation as dependencies. 
+##   
+## ***************************
+
+
+# Plotting and visualization 
+library(ggplot2)
+library(ggsci)
+
+# Data manipulation 
+library(plyr)
+library(dplyr)
+library(patchwork)
+library(tidyverse)
+library(magrittr)
+
+# Spatial transcriptomics analysis 
+library(Seurat)
+library(glmGamPoi)
+library(presto) # this package speeds up the computational time
+library(clustermole) # cell type prediction
+library(iTALK)
+library(CellChat)
+library(liana)
+
+
+# Enrichment analysis 
+library(msigdbr)
+library(clusterProfiler)
+
+
+# rmd rendering 
+library(DT)
+
